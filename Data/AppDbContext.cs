50b7a1e46cc32f17bd4a90f5eaf7e4600fd10d4e
Marcos Farias <fariasmarcos.job03@gmail.com>
2025-05-15 11:51:08 -0300

    Primer commit limpio con .gitignore

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PruebaTecnicaAPI.Models;
+
+namespace PruebaTecnicaAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Proveedor> Proveedores => Set<Proveedor>();
+        public DbSet<Lubricante> Lubricantes => Set<Lubricante>();
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+using PruebaTecnicaAPI.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
+        options.JsonSerializerOptions.WriteIndented = true;
+    });
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Prueba API", Version = "v1" });
+});
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Prueba API V1");
+});
+
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
